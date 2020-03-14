1719ea468cd60130d99101899cbdbf8bb0d5826b
mouss <mouss2388@gmail.com>
2020-03-14 14:16:38 +0100

    ngIf + then + else

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,21 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-              <h2 *ngIf="displayName; else elseBlock">
-                Codevolutions
-              </h2>
+              <div *ngIf="displayName; then thenBlock; else elseBlock"></div>
+              <ng-template #thenBlock>
+                <h2>Codevolution</h2>
+              </ng-template>
 
               <ng-template #elseBlock>
-                <h2>
-                  Name is hidden
-                </h2>
+                <h2>Hidden</h2>
               </ng-template>
               `,
   styles: []
 })
 export class TestComponent implements OnInit {
 
-  displayName = false;
+  displayName = true;
   constructor() { }
 
   ngOnInit() {
