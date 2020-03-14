658314392f2277c01d3b3c48e5a843cff51a581b
mouss <mouss2388@gmail.com>
2020-03-14 18:48:38 +0100

    Project Base

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: `
+      <h2></h2>
+  `,
+  styles: []
+})
+export class TestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
