e38e8c1a32221e5415fe4f424da58c22492d263b
mouss <mouss2388@gmail.com>
2020-03-15 11:08:45 +0100

    Example routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { DepartmentListComponent } from './department-list/department-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'departments', component: DepartmentListComponent},
+  {path: 'employees', component: EmployeeListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [ DepartmentListComponent, EmployeeListComponent ];
